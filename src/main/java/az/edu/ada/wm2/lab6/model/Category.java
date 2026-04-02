76b085c5440c12da20385358782fc25f5a377f5e
BadalovSanan <sbadalov20576@ada.edu.az>
2026-04-02 04:20:23 +0400

    Add CategoryEntity and CategoryRepository

---
+++ src/main/java/az/edu/ada/wm2/lab6/model/Category.java
@@ -0,0 +1,27 @@
+package az.edu.ada.wm2.lab6.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Set;
+import java.util.UUID;
+import java.az.edu.ada.wm2.lab6.model.Product;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+
+    // This is the back reference to products
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products;
+}
--- src/main/java/az/edu/ada/wm2/lab6/model/Product.java
+++ src/main/java/az/edu/ada/wm2/lab6/model/Product.java
@@ -3,7 +3,9 @@ package az.edu.ada.wm2.lab6.model;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Set;
 import java.util.UUID;
+import java.az.edu.ada.wm2.lab6.model.Category;
 
 @Entity
 @Table(name = "products")
@@ -64,4 +66,11 @@ public class Product {
     public void setExpirationDate(LocalDate expirationDate) {
         this.expirationDate = expirationDate;
     }
+    @ManyToMany
+    @JoinTable(
+        name = "product_category",
+        joinColumns = @JoinColumn(name = "product_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories;
 }
