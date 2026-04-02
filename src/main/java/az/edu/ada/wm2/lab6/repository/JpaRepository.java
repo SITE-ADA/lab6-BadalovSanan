e8005d6b0421145970c019e8a8ee6475f0e1b986
BadalovSanan <sbadalov20576@ada.edu.az>
2026-04-02 14:59:06 +0400

    Add CategoryEntity and CategoryRepository

--- src/main/java/az/edu/ada/wm2/lab6/repository/JpaRepository.java
+++
@@ -1,5 +0,0 @@
-package az.edu.ada.wm2.lab6.repository;
-
-public interface JpaRepository<T1, T2> {
-
-}
--- src/main/java/az/edu/ada/wm2/lab6/repository/ProductRepository.java
+++ src/main/java/az/edu/ada/wm2/lab6/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package az.edu.ada.wm2.lab6.repository;
 
 import az.edu.ada.wm2.lab6.model.Product;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 }
